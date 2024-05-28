1c5f1a5e432bd9ded49abee8b309ab05454939dd
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-05-29 01:31:46 +0200

    Updated privacy policy

--- src/components/legal/Privacy.js
+++ src/components/legal/Privacy.js
@@ -40,6 +40,19 @@ function Privacy() {
                     <h2>Data Security</h2>
                     <p>We take reasonable measures to protect the security of your personal information and prevent unauthorized access, disclosure, or alteration. However, no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee absolute security.</p>
 
+                    <h2>Data Protection</h2>
+                    <p>We prioritize your privacy and the protection of your personal information. In compliance with the Protection of Personal Information Act (PoPI) of South Africa, we ensure that your personal data is handled with the utmost care and security.</p>
+                    
+                    <p>Our software utilizes advanced large language models (LLMs) to provide you with tailored legal information and guidance. However, we want to assure you that:</p>
+                    <ul>
+                        <li>No personal data is sent to or processed by the LLMs.</li>
+                        <li>All data processing occurs within our secure, local environment, ensuring that your personal information remains confidential and protected.</li>
+                        <li>We implement robust security measures to safeguard your data against unauthorized access, disclosure, or misuse.</li>
+                    </ul>
+                    <p>By using AI Legal Companion, you can trust that your privacy is our priority and that we adhere to all legal requirements to protect your personal information.</p>
+                    
+                    <p>If you have any questions or concerns regarding our data protection practices, please feel free to contact us.</p>
+
                     <h2>Your Rights</h2>
                     <p>You have the right to access, correct, or delete your personal information. If you would like to exercise these rights or have any questions about our privacy practices, please contact us at <a className='link' href="mailto:menityapps@gmail.com">menityapps@gmail.com</a>.</p>
 
@@ -49,7 +62,7 @@ function Privacy() {
                     <h2>Contact Us</h2>
                     <p>If you have any questions or concerns about our Privacy Policy or data practices, please contact us at <a className='link' href="mailto:menityapps@gmail.com">menityapps@gmail.com</a>.</p>
 
-                    <p>This Privacy Policy was last updated on 14 May 2024.</p>
+                    <p>This Privacy Policy was last updated on 29 May 2024.</p>
                 </div>
             </div>
             <AppFooter />
