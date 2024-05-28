612708d9c3e77e8a5591ca297c284e1f0dc7ee2c
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-05-29 00:32:23 +0200

    Made improvements to the look and feel of the compliance report

--- src/components/upload/ComplianceReport.js
+++ src/components/upload/ComplianceReport.js
@@ -1,3 +1,4 @@
+import './ComplianceReport.css';
 import React, { useState } from 'react';
 import Rating from '@mui/material/Rating';
 import Accordion from '@mui/material/Accordion';
@@ -5,6 +6,10 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import ReportIcon from '@mui/icons-material/Report';
+import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
+import InfoIcon from '@mui/icons-material/Info';
 
 function ComplianceReport(props) {
     const report = props.report;
@@ -16,12 +21,23 @@ function ComplianceReport(props) {
 
     return (
         <div>
-            <h3 className='section-header'>Overall Rating</h3>
+            <div className='section-header'>
+                <InfoIcon className='icon-info' /><h3>&nbsp;Insight</h3>
+            </div>
+            <Typography>I analyzed the document for compliance with South African labour laws.
+                Non-compliant and compliant sections are highlighted.
+                For non-compliant sections, I provided explanations of the issues and added suggestions for improvements.</Typography>
+
+            <div className='section-header'>
+                <LocalPoliceIcon className='icon-rating' /><h3>&nbsp;Overall Rating</h3>
+            </div>
             <Rating name="compliance-rating" defaultValue={report.overall_compliance_rating / 10} max={10} precision={0.1} readOnly />
 
-            <h3 className='section-header'>Non-Compliant Sections</h3>
+            <div className='section-header'>
+                <ReportIcon className='icon-non-compliant' /><h3>&nbsp;Non-Compliant</h3>
+            </div>
             {report.non_compliant_sections.map((section, index) => (
-                <Accordion expanded={expanded === `panel${index}`} onChange={handleChange(`panel${index}`)} key={index}>
+                <Accordion className='accordion-non-compliant' expanded={expanded === `panel${index}`} onChange={handleChange(`panel${index}`)} key={index}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1bh-content"
@@ -46,9 +62,11 @@ function ComplianceReport(props) {
                 </Accordion>
             ))}
 
-            <h3 className='section-header'>Compliant Sections</h3>
+            <div className='section-header'>
+                <VerifiedIcon className='icon-compliant' /><h3>&nbsp;Compliant</h3>
+            </div>
             {report.compliant_sections.map((section, index) => (
-                <Accordion expanded={expanded === `panelb${index}`} onChange={handleChange(`panelb${index}`)} key={index}>
+                <Accordion className='accordion-compliant' expanded={expanded === `panelb${index}`} onChange={handleChange(`panelb${index}`)} key={index}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1bh-content"
